c7c39f86053b3359cacd961ff2e3e7f228bae143
HabiburRahmanAnik <habib264521@gmail.com>
2022-04-03 23:18:25 +0600

    home responsive done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,12 @@ import camera from '../../assest/images/camera.jpg';
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-2 gap-10 px-20 py-24 place-items-center">
+    <div className="grid md:grid-cols-2 text-center md:text-left gap-10 px-8 py-10 md:px-20 md:py-24 place-items-center">
       <div>
-        <h1 className="text-4xl uppercase font-semibold text-blue-400">
+        <h1 className="text-2xl md:text-4xl uppercase font-semibold text-blue-400">
           Canon, EOS R6
         </h1>
-        <p className="mt-5 text-gray-700 text-lg">
+        <p className="mt-5 text-gray-700 md:text-lg">
           This is one of the most beautiful and productive cameras in the world.
           This camera is for professional photographers. You can consider
           yourself lucky if you have such a camera.
