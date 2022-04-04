17b4b1e9709f87e8ab172b62dd37a45038933174
HabiburRahmanAnik <habib264521@gmail.com>
2022-04-04 16:47:31 +0600

    add sementic tag qn ans

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Blogs = () => {
   return (
     <div className="p-8 md:px-40 md:py-14">
-      <div className="border-2 px-8 rounded py-3 text-gray-700">
-        <h1 className="text-2xl mb-2 font-medium">What is context api?</h1>
+      <div className="border-2 border-blue-100 px-8 rounded py-3 text-gray-700 mb-6">
+        <h1 className="text-2xl mb-2 font-medium">What is context API?</h1>
         <p>
           With the help of React context, we can pass and use data on whatever
           component we need in our React app. Context API is helpful when we
@@ -14,6 +14,27 @@ const Blogs = () => {
           Context API helps us avoid this type of issue( props drilling). This
           is the benefit of context API – we can pass data anywhere in our react
           app to avoid the props drilling.
+        </p>
+      </div>
+      <div className="border-2 border-blue-100 px-8 rounded py-3 text-gray-700">
+        <h1 className="text-2xl mb-2 font-medium">What is semantic tag?</h1>
+        <p>
+          Semantic tags are HTML elements those clearly describe their meaning
+          in a human and machine-readable way. Some of semantic tags are:
+          <span className="bg-blue-50 mx-1">articel</span>,
+          <span className="bg-blue-50 mx-1">aside</span>,
+          <span className="bg-blue-50 mx-1">details</span>,
+          <span className="bg-blue-50 mx-1">nav</span>,
+          <span className="bg-blue-50 mx-1">header</span>,
+          <span className="bg-blue-50 mx-1">articel</span>,
+          <span className="bg-blue-50 mx-1">section</span>,
+          <span className="bg-blue-50 mx-1">figure</span>,
+          <span className="bg-blue-50 mx-1">figcaption</span>,
+          <span className="bg-blue-50 mx-1">footer</span> etc. Some of the
+          benefits from writing semantic markup are we can easliy understend
+          what is the menaing in this tag, SEO can be done properly, Screen
+          readers can use it as a signpost to help visually impaired users
+          navigate a page and also it is much easier to read.
         </p>
       </div>
     </div>
